7031e402fbe2644c574bb8dc12dc5fed371f42f6
Luong <Julia.Luong@fnf.com>
2025-05-13 13:00:34 -0700

    Progress commit

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // Return "ERROR" on non-integers, non-numbers, and negative numbers
+    
 
+    let result = 0;
+    for (let i = num1; i <= num2; i++) {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
