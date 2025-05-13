28de49d9fdfbb0085c872d19bec92773d15ef33f
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-05-12 21:36:11 -0700

    Complete exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    for (element of toRemove) {
+        let i = 0;
+        while (i < arr.length) {
+            if (arr[i] !== element){
+                i++;
+                continue;
+            }
+            arr.splice(i, 1);
+        }
+    }
+    return arr;
 };
+
+console.log(removeFromArray([1,2,2,3], 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
