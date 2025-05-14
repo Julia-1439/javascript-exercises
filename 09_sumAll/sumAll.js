c86325cc079f492dc3bab91775e3b018458f7890
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-05-13 20:41:11 -0700

    Complete exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,16 @@
 const sumAll = function(num1, num2) {
     // Return "ERROR" on non-integers, non-numbers, and negative numbers
-    
+    for (arg of arguments) {
+        if (typeof arg !== "number" || arg < 0 || 
+            Math.floor(arg) !== arg)
+            return "ERROR";
+    }
+    // Swap if the first number is larger than the second
+    if (num1 > num2) {
+        const temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
 
     let result = 0;
     for (let i = num1; i <= num2; i++) {
