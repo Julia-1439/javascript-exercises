ab6ca3d43a416ddcecc1708e2a148dd92e56720d
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-05-17 20:06:22 -0700

    Complete exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+// Convert a temperature in F to C rounded to 1 decimal place
+const convertToCelsius = function(tempF) {
+  const roundingFactor = 10;
+  return Math.round(
+    ((tempF - 32) * 5/9) * roundingFactor
+  ) / roundingFactor;
 };
 
-const convertToFahrenheit = function() {
+// Convert a temperature in F to C rounded to 1 decimal place
+const convertToFahrenheit = function(tempC) {
+  const roundingFactor = 10;
+  return Math.round(
+    (tempC * 9/5 + 32) * roundingFactor
+  ) / roundingFactor;
 };
 
 // Do not edit below this line
