1b23aa9aff3ab442e60a79529d473edceb816b58
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-06-28 13:08:49 -0700

    Complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    index = +index;
+    
+    if (index < 0) return "OOPS";
+    if (index === 0) return 0;
+    if (index === 1 || index === 2) return 1;
+
+    let [secondPrev, firstPrev] = [1, 1];
+    for (let i = 0; i < index - 2; i++) {
+        const curr = secondPrev + firstPrev;
+        secondPrev = firstPrev;
+        firstPrev = curr;
+    }
+
+    return firstPrev;
 
 };
 
