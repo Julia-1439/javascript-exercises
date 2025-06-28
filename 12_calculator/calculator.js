8c2385cf2a75f5a4f91b81af50213f5337c5e0fd
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-06-28 11:58:52 -0700

    Complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(operands) {
+	return operands.reduce((result, num) => result + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(operands) {
+	return operands.reduce((result, num) => result * num);  
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1; 
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
