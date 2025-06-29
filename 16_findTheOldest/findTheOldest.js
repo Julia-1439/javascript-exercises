ac01d14fedefad24f80afce9a6cd012321c10584
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-06-28 17:16:38 -0700

    Complete exercise 16 and wrote alt soln

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,36 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    /* OLD SOLUTION
+    return people.reduce(
+        (oldest, person) => {
+            const presentYear = (new Date()).getFullYear();
 
+            const oldestAge = 
+                ("yearOfDeath" in oldest ? oldest.yearOfDeath : presentYear)
+                - oldest.yearOfBirth
+            const personAge = 
+                ("yearOfDeath" in person ? person.yearOfDeath : presentYear)
+                - person.yearOfBirth
+
+            return (personAge > oldestAge) ? person : oldest;
+        }
+    );
+    */
+
+    // NEW SOLUTION using the nullish coalescing operator and a helper function!
+    return people.reduce(
+        (oldestPerson, person) => {
+            const oldestPersonAge = getAge(oldestPerson);
+            const personAge = getAge(person);
+            return (oldestPersonAge < personAge) ? person : oldestPerson;
+        }  
+    );
+
+};
+
+const getAge = function(person) {
+    // This is shorthand for person.deathYear = person.deathYear ?? ...
+    person.yearOfDeath ??= new Date().getFullYear();
+    return person.yearOfDeath - person.yearOfBirth
 };
 
 // Do not edit below this line
